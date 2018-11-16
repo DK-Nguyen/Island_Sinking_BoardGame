9ee5029481e226f949a5402f8c6915103b61bd7b
Khoa Nguyen <nguyen29@student.tut.fi>
2018-11-16 19:53:05 +0200

    tet constructor of Gamestate

--- Tests/UnitTests/Gamestate/tst_gamestatetest.cpp
+++ Tests/UnitTests/Gamestate/tst_gamestatetest.cpp
@@ -1,6 +1,11 @@
 #include <QtTest>
 
 // add necessary includes here
+#include <gamestate.hh>
+#include <gameexception.hh>
+#include <player.hh>
+#include <memory>
+#include "iplayer.hh"
 
 class gamestatetest : public QObject
 {
@@ -11,24 +16,36 @@ public:
     ~gamestatetest();
 
 private slots:
-    void test_case1();
+    void initGamestate();
 
 };
 
 gamestatetest::gamestatetest()
 {
-
 }
 
 gamestatetest::~gamestatetest()
+{   
+}
+
+void gamestatetest::initGamestate()
 {
+    std::shared_ptr<std::unordered_map<int, std::string>> player_names = std::make_shared<std::unordered_map<int, std::string>>();
+    std::shared_ptr<std::unordered_map<std::string, int>> points = std::make_shared<std::unordered_map<std::string, int>>();
+    std::shared_ptr<std::vector<std::pair<std::string, int>>> top10 =  std::make_shared<std::vector<std::pair<std::string, int>>>();
+    std::vector<std::shared_ptr<Common::IPlayer>> players;
+//    auto player_ptr = std::make_shared<Player>(0);
+//    auto player_ptr2 = std::make_shared<Player>(1);
+//    players.push_back(player_ptr);
+//    players.push_back(player_ptr2);
+
+    std::shared_ptr<GameState> game_state = std::make_shared<GameState>(Common::GamePhase(1), 1, players, player_names, points, top10);
+    QCOMPARE(game_state->currentGamePhase(), Common::GamePhase(1));
+    QCOMPARE(game_state->currentPlayer(), 1);
+
 
 }
 
-void gamestatetest::test_case1()
-{
-
-}
 
 QTEST_APPLESS_MAIN(gamestatetest)
 
